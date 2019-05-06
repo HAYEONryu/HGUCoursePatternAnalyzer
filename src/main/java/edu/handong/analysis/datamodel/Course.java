97da7ea4ee522d12f83c20c94121f1b80ae23772
HAYEONryu <21700470@handong.edu>
2019-05-06 20:43:59 +0900

    Initial commit_add datamodel package

---
+++ src/main/java/edu/handong/analysis/datamodel/Course.java
@@ -0,0 +1,11 @@
+package edu.handong.analysis.datamodel;
+
+public class Course {
+	String courseName;
+	public Course(String name) {
+		courseName = name;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+}
